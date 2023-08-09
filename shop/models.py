818b6e4db0dc7ed878653ca7fd41ed86b77f8ae7
Dawid Kapłon <dawidkaplon2@gmail.com>
2023-08-09 16:17:01 +0200

    Add .gitignore

--- shop/models.py
+++ shop/models.py
@@ -8,6 +8,7 @@ class Item(models.Model):
     description = models.TextField(default='')
     price = models.FloatField()
     category = models.TextField(default='')
+    image = models.ImageField(upload_to='item_images/')
 
     def __str__(self):
         return self.name
